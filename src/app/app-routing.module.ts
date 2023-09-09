2a1350a9561ee1e58e390a5712c458399882a7d1
tronghenry273 <101923224+QuiBuuIzTheBest@users.noreply.github.com>
2023-09-09 16:33:31 +0700

    12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './contents/page-not-found/page-not-found.component';
+import { HomeComponent } from './contents/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
